5bc8fe57903364f8e5f0e84faf7e0a5f83d140fc
regisatl <regisattolou21@outlook.fr>
2023-10-26 16:06:13 +0200

    First Commit With Routes and Migrations of Database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('articles')->name('blog.')->group(function() {
+
+    Route::get('/', function() {
+        return
+        [
+            'Link'  => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+        ];
+    })->name('blog.index');
+
+    Route::get('/{slug}-{id}', function(string $slug, string $id, Request $request) {
+        return [
+            'slug' => $slug,
+            'id' => $id,
+            'name' => $request ->input('name')
+        ];
+    })->where([
+        'id' => '[0-9]+',
+        'slug' => '[a-z0-9\-]+',
+    ])->name('blog.show');
+
 });
